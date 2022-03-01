c7cc29b70913f7cb617c22b339a1ef7959583010
Henry-Azer <henaryazer@outlook.com>
2022-03-01 14:45:24 +0200

    Initial commit

---
+++ src/test/java/com/henry/jwt/SpringJwtStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henry.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
